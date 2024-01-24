8b783192e448d8245d79938ebf6a41d4ae440dd1
Huỳnh Đức <hiepthaitran2001@gmail.com>
2024-01-24 14:50:43 +0700

    create project and config database

---
+++ WebBE/src/main/java/com/huynhduc/WebBE/WebBeApplication.java
@@ -0,0 +1,13 @@
+package com.huynhduc.WebBE;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebBeApplication.class, args);
+	}
+
+}
---
+++ WebBE/src/test/java/com/huynhduc/WebBE/WebBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.huynhduc.WebBE;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
